094c8341669a121b32742d173cd7cba01840e286
buddybuild <help@buddybuild.com>
2016-09-10 16:24:11 -0700

    Adding buddybuild SDK
    - Add BuddyBuildSDK.framework
    - Initialize in App Delegate

--- my-reddit/Classes/AppDelegate.m
+++ my-reddit/Classes/AppDelegate.m
@@ -27,11 +27,14 @@
 
 #import "AppDelegate.h"
 #import "MainViewController.h"
+#import <BuddyBuildSDK/BuddyBuildSDK.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions
 {
+    [BuddyBuildSDK setup];
+    
     self.viewController = [[MainViewController alloc] init];
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
